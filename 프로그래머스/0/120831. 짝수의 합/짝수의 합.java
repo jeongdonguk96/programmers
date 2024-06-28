2092fda654a8938fe84a67d43fc7c8555a262a00
DongUk <110921983+jeongdonguk96@users.noreply.github.com>
2024-06-29 01:09:54 +0900

    [level 0] Title: 짝수의 합, Time: 1.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -3,7 +3,7 @@ import java.util.stream.IntStream;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        return IntStream.range(0, n + 1)
+        return IntStream.rangeClosed(0, n)
                 .filter(i -> i % 2 == 0)
                 .sum();
     }
