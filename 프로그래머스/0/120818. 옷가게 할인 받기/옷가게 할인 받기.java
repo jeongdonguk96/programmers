2601642996631fc953b9b42a63def355f4cba5c7
DongUk <110921983+jeongdonguk96@users.noreply.github.com>
2024-06-29 20:40:39 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,12 +1,8 @@
 class Solution {
     public int solution(int price) {
-        if (price >= 500000) {
-            price -= price * 0.20;
-        } else if (price >= 300000) {
-            price -= price * 0.10;
-        } else if (price >= 100000) {
-            price -= price * 0.05;
-        }
+        if(price >= 500000) return (int) (price * 0.80);
+        if(price >= 300000) return (int) (price * 0.90);
+        if(price >= 100000) return (int) (price * 0.95);
         
         return price;
     }
