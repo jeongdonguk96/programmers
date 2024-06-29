e4d6d03d79d6c1eea599a525d9b6977bdac36c1d
DongUk <110921983+jeongdonguk96@users.noreply.github.com>
2024-06-29 14:22:09 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n + 1) / 2];
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                answer[i/2] = i;
+            }
+        }
+        
+        return answer;
+    }
+}
