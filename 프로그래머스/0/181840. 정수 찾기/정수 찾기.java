ce3ebbb951b626673a04a432f9d79452725d9df8
DongUk <110921983+jeongdonguk96@users.noreply.github.com>
2024-07-02 15:20:38 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,13 @@
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        for (int i = 0; i < num_list.length; i++) {
+            if (n == num_list[i]) {
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+}
