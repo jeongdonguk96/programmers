58781c2e73f1e73737475ee78ac7c2118eb69b88
DongUk <110921983+jeongdonguk96@users.noreply.github.com>
2024-07-03 22:00:03 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 8.43 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        return k % 2 == 0 
+                ? Arrays.stream(arr)
+                    .map(i -> i + k)
+                    .toArray()
+                : Arrays.stream(arr)
+                    .map(i -> i * k)
+                    .toArray();
+    }
+}
