3b213aa8bb234e1c6aa4cdbebff5fcd6e8f77b47
DongUk <110921983+jeongdonguk96@users.noreply.github.com>
2024-07-07 19:55:10 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int size = n / k;
+        int[] answer = new int[size];
+        int index = 0;
+        for (int i = k; i <= n; i+=k) {
+            answer[index] = i;
+            index++;
+        }
+        
+        return answer;
+    }
+}
