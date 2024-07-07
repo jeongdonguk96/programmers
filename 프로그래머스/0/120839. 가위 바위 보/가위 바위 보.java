a504222e3fefbd7528d2079b8a99b7743fee0dc7
DongUk <110921983+jeongdonguk96@users.noreply.github.com>
2024-07-07 17:43:17 +0900

    [level 0] Title: 가위 바위 보, Time: 1.81 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,12 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String rsp) {
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < rsp.length(); i++) {
-            String str = "2".equals(String.valueOf(rsp.charAt(i))) ? "0" : ("0".equals(String.valueOf(rsp.charAt(i))) ? "5" : "2");
-            sb.append(str);
-        }
-
-        return sb.toString();
+        return Arrays.stream(rsp.split(""))
+            .map(str -> str.equals("2") ? "0" : (str.equals("0") ? "5" : "2"))
+            .collect(Collectors.joining());
     }
 }
